943c70df8dc7a8add2f062db315ea9e85c3280fb
Mohit <arceus93007@gmail.com>
2026-06-27 23:29:25 +0530

    Database connected

---
+++ Config/database.js
@@ -0,0 +1,13 @@
+const mongoose=require("mongoose");
+require("dotenv").config();
+
+const dbConnect=()=>{
+  mongoose.connect(process.env.DATABASE_URL)
+  .then(()=>{console.log("DB connection successful")})
+  .catch((error)=>{
+    console.log("Issue in DB connection");
+    process.exit(1);
+  })
+}
+
+module.exports=dbConnect;
